ffbf69c1c296c0d47ec94e9bf8fc1a58228f58ac
toka404 <58258902+toka404@users.noreply.github.com>
2022-05-15 15:27:20 -0500

    niveles

--- Assets/Scripts/BotonGeiser.cs
+++ Assets/Scripts/BotonGeiser.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class BotonGeiser : MonoBehaviour
 {
     [SerializeField] GameObject graficos;
-    Animator animatorGeiser;
 
     public bool estado;
 
     // Start is called before the first frame update
     void Start()
     {
-        animatorGeiser = graficos.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,13 +21,11 @@ public class BotonGeiser : MonoBehaviour
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        animatorGeiser.Play("inicio");
         estado = true;
     }
 
     public void OnTriggerExit2D(Collider2D collision)
     {
         estado = false;
-        animatorGeiser.Play("parar");
     }
 }
