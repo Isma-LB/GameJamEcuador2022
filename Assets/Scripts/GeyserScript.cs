8601438ff1964f5bea264f72d24aac3344b5ee4e
IsmaLB <bravoismael26@gmail.com>
2022-05-15 15:27:00 -0500

    Graphics

--- Assets/Scripts/GeyserScript.cs
+++ Assets/Scripts/GeyserScript.cs
@@ -10,11 +10,17 @@ public class GeyserScript : MonoBehaviour
     [SerializeField] BotonGeiser boton2;
     [SerializeField] BotonGeiser boton3;
 
+    ParticleSystem geizerParticles;
+    ParticleSystem geizer2Particles;
+
     // Start is called before the first frame update
     void Start()
     {
         geizer.enabled = false;
         geizer2.enabled = false;
+
+        geizerParticles = geizer.GetComponent<ParticleSystem>();
+        geizer2Particles = geizer2.GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
